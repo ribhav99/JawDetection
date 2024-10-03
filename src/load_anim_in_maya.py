23f55ea3247ef31861b7bfb1cf8894d554e689b3
Ribhav Kapur <ribhav.kapur99@gmail.com>
2024-10-03 11:17:25 -0400

    Update load_anim_in_maya.py

--- src/load_anim_in_maya.py
+++ src/load_anim_in_maya.py
@@ -9,7 +9,7 @@ def load_animation_from_pt(pt_file_path):
     attribute_to_modify = "usr_CNT_JAW.Open_jaw"
     for i in range(len(data)):
         cmds.currentTime(i, edit=True)
-        cmds.setKeyframe(attribute_to_modify, value=data[i])
+        cmds.setKeyframe(attribute_to_modify, value=data[i].item())
 
 load_animation_from_pt(os.path.join(os.path.dirname(__file__), "../new_outputs/KYLE-D1-012.pt"))
     
